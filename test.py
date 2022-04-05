fdb7491e6a41925db88af404c46d8f7893679fa9
you know who <chorvickprocode@gmail.com>
2022-04-05 19:02:42 -0400

    python test driven development

---
+++ test.py
@@ -0,0 +1,42 @@
+import pytest
+from TDD.challenges.strings import count_vowels, is_palindrome, reverse_string
+
+
+@pytest.mark.parametrize(
+    "name, expected",
+    [
+        ("", 0),
+        ("a", 1),
+        ("ab", 1),
+        ("abcde", 2)
+    ],
+)
+def test_count_vowels(name, expected):
+    assert count_vowels(name) == expected
+
+
+@pytest.mark.parametrize(
+    "string, expected",
+    [
+        ("", True),
+        ("a", True),
+        ("ab", False),
+        ("abcde", False),
+        ("abcba", True)
+    ],
+)
+def test_is_palindrome(string, expected):
+    assert is_palindrome(string) == expected
+
+
+@pytest.mark.parametrize(
+    "string, expected",
+    [
+        ("", ""),
+        ("a", "a"),
+        ("ab", "ba"),
+        ("abcde", "edcba")
+    ],
+)
+def test_reverse_string(string, expected):
+    assert reverse_string(string) == expected
